8b4c6b96dbbf8aa92a7e44cc082a0840da33af15
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-08-28 11:19:45 -0700

    all part 1 and setting up of libraries model in part 2

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, confirmation: true
+
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user ? @user.authenticate(params[:password]) : false
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "users#index"
+  get "/users/new", to: "users#new", as: "new_user"
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  get '/login', to: "sessions#new"
+  get '/logout', to: 'sessions#destroy'
+  post '/sessions', to: "sessions#create"
+
+  get '/libraries', to: 'libraries#index'
+  get '/libraries/new', to: 'libraries#new', as: 'new_library'
+  post '/libraries', to: 'libraries#create'
+  get '/libraries/:id', to: 'libraries#show', as: 'library'
 end
